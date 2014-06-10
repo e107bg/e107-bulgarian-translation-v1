3fc8f8be2ed37a9fcf7c521f39391264ae56a1b0
Veselin Totev <v.totev@clabteam.com>
2014-06-10 13:28:02 +0300

    moving from SVN to GitHub

---
+++ e107_languages/Bulgarian/lan_equery_secure.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * e107 website system - Bulgarian Translation
+ *
+ * Copyright (C) 2005-2011 e107 Bulgaria e107.bg
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * $Id$
+*/
+define("EQSEC_LAN1", "Вие ще бъдете пренасочени към администраторска функция, възможно е да се направи промяна в базата данни");
+define("EQSEC_LAN2", "Моля, потвърдете:");
+define("EQSEC_LAN3", "Без пренасочване");
+define("EQSEC_LAN4", "Действие от:");
+define("EQSEC_LAN5", "Действие към:");
+define("EQSEC_LAN6", "Потвърждение");
+define("EQSEC_LAN7", "или отказ");
